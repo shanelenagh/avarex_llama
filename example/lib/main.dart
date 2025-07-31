b6201fa1227fc1fa2ba7ab20dd4687aeef1e5d66
Shane Lenagh <shanelenagh@gmail.com>
2025-07-31 15:02:45 -0500

    More progress, and gitignoreing some distracting build files

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,74 +1,14 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:avarex_llama/avarex_llama.dart' as avarex_llama;
+import 'dart:ffi';
+import 'package:avarex_llama/avarex_llama_bindings_generated.dart' as avarex_llama;
+import 'dart:io';
+import 'package:path/path.dart' as p;
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  late int sumResult;
-  late Future<int> sumAsyncResult;
-
-  @override
-  void initState() {
-    super.initState();
-    sumResult = avarex_llama.sum(1, 2);
-    sumAsyncResult = avarex_llama.sumAsync(3, 4);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    const textStyle = TextStyle(fontSize: 25);
-    const spacerSmall = SizedBox(height: 10);
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Native Packages'),
-        ),
-        body: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              children: [
-                const Text(
-                  'This calls a native function through FFI that is shipped as source in the package. '
-                  'The native code is built as part of the Flutter Runner build.',
-                  style: textStyle,
-                  textAlign: TextAlign.center,
-                ),
-                spacerSmall,
-                Text(
-                  'sum(1, 2) = $sumResult',
-                  style: textStyle,
-                  textAlign: TextAlign.center,
-                ),
-                spacerSmall,
-                FutureBuilder<int>(
-                  future: sumAsyncResult,
-                  builder: (BuildContext context, AsyncSnapshot<int> value) {
-                    final displayValue =
-                        (value.hasData) ? value.data : 'loading';
-                    return Text(
-                      'await sumAsync(3, 4) = $displayValue',
-                      style: textStyle,
-                      textAlign: TextAlign.center,
-                    );
-                  },
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
+  final summer = avarex_llama.AvarexLlamaBindings(DynamicLibrary.open(
+    p.join(p.current, "llama.dll")));
+  final sumResult = summer.sum(1, 2);
+  stdout.write("Hi: "+sumResult.toString());
+  stdout.write("Starting llama...");
+  summer.start_llama();
+  stdout.write("Started llama");
 }
