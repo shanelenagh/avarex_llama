c1c05d8bcf7faba8bec20395763fedbeb1361b54
Shane Lenagh <shanelenagh@gmail.com>
2025-08-01 14:27:30 -0500

    Working full llama_load

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,14 +1,11 @@
-import 'dart:ffi';
+import 'dart:ffi' as ffi;
+import 'package:ffi/ffi.dart';
 import 'package:avarex_llama/avarex_llama_bindings_generated.dart' as avarex_llama;
-import 'dart:io';
 import 'package:path/path.dart' as p;
 
 void main() {
-  final summer = avarex_llama.AvarexLlamaBindings(DynamicLibrary.open(
-    p.join(p.current, "llama.dll")));
-  final sumResult = summer.sum(1, 2);
-  stdout.write("Hi: "+sumResult.toString());
-  stdout.write("Starting llama...");
-  summer.start_llama();
-  stdout.write("Started llama");
+  final llamacpp_dll = avarex_llama.AvarexLlamaBindings(ffi.DynamicLibrary.open(p.join(p.current, "llama.dll")));
+  print("Starting llama...");
+  llamacpp_dll.start_llama("granite-3.3-2b-instruct-Q5_1.gguf".toNativeUtf8().cast<ffi.Char>());
+  print("Started llama");
 }
