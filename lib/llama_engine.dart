b3a249561a626d2e646965382ed3d4db5b4b88c8
Shane Lenagh <shanelenagh@gmail.com>
2025-08-05 10:41:01 -0500

    Completed model, context, and sampler config refactoring

--- lib/llama_engine.dart
+++ lib/llama_engine.dart
@@ -9,17 +9,15 @@ class LlamaEngine {
   final _log = Logger((LlamaEngine).toString());
   final avarex_llama.AvarexLlamaBindings _llamacppLib = avarex_llama.AvarexLlamaBindings(ffi.DynamicLibrary.open("llama.dll"));
 
-  LlamaEngine(String modelPath, avarex_llama.llama_model_params? pmodelParams) {   
+  LlamaEngine(String modelPath, ModelConfig? modelConfig) {   
     _log.info("Starting llama.cpp with model $modelPath...");
-    avarex_llama.llama_model_params modelParams = pmodelParams ?? _llamacppLib.get_default_model_params();
-    modelParams.n_gpu_layers = 99;
-    _llamacppLib.start_llama(_dart2ffi(modelPath), modelParams);
+    _llamacppLib.start_llama(_dart2ffi(modelPath), (modelConfig??ModelConfig())._getLlamaModelParams(_llamacppLib));
     _log.info("Started llama.cpp");
   }
 
-  String runGeneration(String prompt, int nPredict, avarex_llama.llama_context_params? contextParams, avarex_llama.llama_sampler_chain_params? samplerParams) {
+  String runGeneration(String prompt, int nPredict, ContextConfig? contextConfig, SamplerConfig? samplerConfig) {
     return _ffi2dart(_llamacppLib.run_generation(_dart2ffi(prompt), nPredict, 
-      contextParams ?? _llamacppLib.get_default_context_params(), samplerParams ?? _llamacppLib.get_default_sampler_params()));
+      (contextConfig??ContextConfig())._getLlamaContextParams(_llamacppLib), (samplerConfig??SamplerConfig())._getLlamaSamplerParams(_llamacppLib)));
   }
   
   @pragma("vm:prefer-inline")
@@ -31,5 +29,63 @@ class LlamaEngine {
     final String dartString = ptr.cast<Utf8>().toDartString();
     _llamacppLib.free_string(ptr);
     return dartString;
+  }
+}
+
+class ModelConfig {
+  /// Number of model layers to run on the GPU VRAM (greatly improves 
+  /// performance to run as many layers on GPU as possible)
+  int gpuLayers = 99;
+  /// Use memory mapping
+  bool? useMemoryMapping;
+  /// Force system to keep model in RAM
+  bool? lockModelInRam;  
+
+  avarex_llama.llama_model_params _getLlamaModelParams(avarex_llama.AvarexLlamaBindings llamacppLib) {
+    var modelParams = llamacppLib.get_default_model_params();
+    modelParams.n_gpu_layers = gpuLayers;
+    modelParams.use_mmap = useMemoryMapping ?? modelParams.use_mmap;
+    modelParams.use_mlock = lockModelInRam ?? modelParams.use_mlock;
+    return modelParams;
+  }
+}
+
+class ContextConfig {
+  /// Max number of text context tokens
+  int? maxTokenCount;
+  /// Logical max batch size to submit to llama decode
+  int? logicalMaxBatchSize;
+  /// Physical maximum batch size
+  int? physicalMaxBatchSize;
+  /// max number of sequences (i.e. distinct states for recurrent models)
+  int? maxSequences;
+  /// Number of threads to use for generation
+  int? genThreadCount;
+  /// Number of threads to use for batch processing
+  int? batchThreadCount;
+  /// Performance timings enabled
+  bool? performanceTimings;
+
+  avarex_llama.llama_context_params _getLlamaContextParams(avarex_llama.AvarexLlamaBindings llamacppLib) {
+    var ctxParams = llamacppLib.get_default_context_params();
+    ctxParams.n_ctx = maxTokenCount ?? ctxParams.n_ctx;
+    ctxParams.n_batch = logicalMaxBatchSize ?? ctxParams.n_batch;
+    ctxParams.n_ubatch = physicalMaxBatchSize ?? ctxParams.n_ubatch;
+    ctxParams.n_seq_max = maxSequences ?? ctxParams.n_seq_max;
+    ctxParams.n_threads = genThreadCount ?? ctxParams.n_threads;
+    ctxParams.n_threads_batch = batchThreadCount ?? ctxParams.n_threads_batch;
+    ctxParams.no_perf = !(performanceTimings ?? !ctxParams.no_perf);
+    return ctxParams;
+  }
+}
+
+class SamplerConfig {
+  /// Performance timings enabled
+  bool? performanceTimings;
+
+  avarex_llama.llama_sampler_chain_params _getLlamaSamplerParams(avarex_llama.AvarexLlamaBindings llamacppLib) {
+    var samplerParams = llamacppLib.get_default_sampler_params();
+    samplerParams.no_perf = !(performanceTimings ?? !samplerParams.no_perf);
+    return samplerParams;
   }
 }
