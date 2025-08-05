a9c0e573d6af6e53cd1e4b5e0384e5d0fc1d1c41
Shane Lenagh <shanelenagh@gmail.com>
2025-08-05 10:40:59 -0500

    More Linux-friendly test.sh script

--- src/test.sh
+++ src/test.sh
@@ -1,6 +1,10 @@
 cmake .
 cmake --build .
-cp bin/Debug/* Debug/   # Move shared libs in with EXE
-# Now download model, iff it hasn't been downloaded yet
-curl -C - -L https://huggingface.co/ibm-granite/granite-3.3-2b-instruct-GGUF/resolve/main/granite-3.3-2b-instruct-Q5_1.gguf?download=true -o Debug/granite-3.3-2b-instruct-Q5_1.gguf
-./Debug/llama_test.exe Debug/granite-3.3-2b-instruct-Q5_1.gguf "What is the starting height of class A airspace?" 20
+curl -C - -L https://huggingface.co/ibm-granite/granite-3.3-2b-instruct-GGUF/resolve/main/granite-3.3-2b-instruct-Q5_1.gguf?download=true -o granite-3.3-2b-instruct-Q5_1.gguf
+if [[ "$(uname -s)" == "Linux" ]]; then
+    cp llama_test bin/
+    ./bin/llama_test granite-3.3-2b-instruct-Q5_1.gguf "What is the starting height of class A airspace?" 20
+else
+  cp bin/Debug/* Debug/*
+  ./Debug/llama_test granite-3.3-2b-instruct-Q5_1.gguf "What is the starting height of class A airspace?" 20
+fi
