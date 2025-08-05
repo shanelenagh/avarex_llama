3a0ac98011fe48b48f1d7af05e96330607f7da37
Shane Lenagh <shanelenagh@gmail.com>
2025-08-05 10:40:59 -0500

    Another bug in test.sh (/src version)

--- src/test.sh
+++ src/test.sh
@@ -5,6 +5,6 @@ if [[ "$(uname -s)" == "Linux" ]]; then
     cp llama_test bin/
     ./bin/llama_test granite-3.3-2b-instruct-Q5_1.gguf "What is the starting height of class A airspace?" 20
 else
-  cp bin/Debug/* Debug/*
+  cp bin/Debug/* Debug/
   ./Debug/llama_test granite-3.3-2b-instruct-Q5_1.gguf "What is the starting height of class A airspace?" 20
 fi
