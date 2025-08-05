20fa7be94cdf1501c8d50db8a542116615913146
Shane Lenagh <shanelenagh@gmail.com>
2025-08-05 10:40:56 -0500

    WIP

--- lib/avarex_llama_bindings_generated.dart
+++ lib/avarex_llama_bindings_generated.dart
@@ -26,34 +26,21 @@ class AvarexLlamaBindings {
     ffi.Pointer<T> Function<T extends ffi.NativeType>(String symbolName) lookup,
   ) : _lookup = lookup;
 
-  /// A very short-lived native function.
-  ///
-  /// For very short-lived functions, it is fine to call them on the main isolate.
-  /// They will block the Dart execution while running the native function, so
-  /// only do this for native functions which are guaranteed to be short-lived.
-  int sum(int a, int b) {
-    return _sum(a, b);
+  ffi.Pointer<ffi.Char> run_generation(
+    ffi.Pointer<ffi.Char> promptc,
+    int n_predict,
+  ) {
+    return _run_generation(promptc, n_predict);
   }
 
-  late final _sumPtr =
-      _lookup<ffi.NativeFunction<ffi.Int Function(ffi.Int, ffi.Int)>>('sum');
-  late final _sum = _sumPtr.asFunction<int Function(int, int)>();
-
-  /// A longer lived native function, which occupies the thread calling it.
-  ///
-  /// Do not call these kind of native functions in the main isolate. They will
-  /// block Dart execution. This will cause dropped frames in Flutter applications.
-  /// Instead, call these native functions on a separate isolate.
-  int sum_long_running(int a, int b) {
-    return _sum_long_running(a, b);
-  }
-
-  late final _sum_long_runningPtr =
-      _lookup<ffi.NativeFunction<ffi.Int Function(ffi.Int, ffi.Int)>>(
-        'sum_long_running',
-      );
-  late final _sum_long_running = _sum_long_runningPtr
-      .asFunction<int Function(int, int)>();
+  late final _run_generationPtr =
+      _lookup<
+        ffi.NativeFunction<
+          ffi.Pointer<ffi.Char> Function(ffi.Pointer<ffi.Char>, ffi.Int)
+        >
+      >('run_generation');
+  late final _run_generation = _run_generationPtr
+      .asFunction<ffi.Pointer<ffi.Char> Function(ffi.Pointer<ffi.Char>, int)>();
 
   void start_llama(ffi.Pointer<ffi.Char> model_path) {
     return _start_llama(model_path);
---
+++ src/avarex_llama.cpp
@@ -0,0 +1,126 @@
+#include "avarex_llama.hpp"
+#include "llama.h"
+#include "stdio.h"
+#include <string>
+#include <vector>
+
+struct llama_model* model = NULL;
+const struct llama_vocab* vocab = NULL;
+
+FFI_PLUGIN_EXPORT char* run_generation(char* promptc, int n_predict) {
+
+    std::string prompt(promptc);
+
+    if (model == NULL || vocab == NULL) {
+        fprintf(stderr, "Model or vocabulary not initialized. Call start_llama() first, or see why said call failed to initialize.\n");
+        return NULL;
+    }
+
+    // tokenize the prompt, allocating enough space for the tokens
+    const int n_prompt = -llama_tokenize(vocab, prompt.c_str(), prompt.size(), NULL, 0, true, true);
+    std::vector<llama_token> prompt_tokens(n_prompt);
+    if (llama_tokenize(vocab, prompt.c_str(), prompt.size(), prompt_tokens.data(), prompt_tokens.size(), true, true) < 0) {
+        fprintf(stderr, "%s: error: failed to tokenize the prompt\n", __func__);
+        return NULL;
+    }    
+
+    // inittialize the context, sizing according to prompt and allowed prediction token size
+    struct llama_context_params context_params = llama_context_default_params();    
+    context_params.n_ctx = n_prompt + n_predict - 1; // make context big enough to hold the prompt and desired # of prediction tokens
+    context_params.n_batch = n_prompt; // n_batch is the maximum number of tokens that can be processed in a single call to llama_decode
+    context_params.no_perf = false; // enable performance stats
+    struct llama_context* ctx = llama_init_from_model(model, context_params);  
+
+    // create a sampler for the generation
+    auto sparams = llama_sampler_chain_default_params();
+    sparams.no_perf = false; // enable performance stats
+    struct llama_sampler* sampler = llama_sampler_chain_init(llama_sampler_chain_default_params());
+    llama_sampler_chain_add(sampler, llama_sampler_init_greedy());
+
+    // TODO: print the prompt??
+    fprintf(stderr, "PRINTING PROMPT (confirming tokenization): %s\n", prompt.c_str());
+    for (auto id : prompt_tokens) {
+        char buf[128];
+        int n = llama_token_to_piece(vocab, id, buf, sizeof(buf), 0, true);
+        if (n < 0) {
+            fprintf(stderr, "%s: error: failed to convert token to piece\n", __func__);
+            return NULL;
+        }
+        std::string s(buf, n);
+        printf("%s", s.c_str());
+    } 
+
+    // prepare a batch for the prompt
+    llama_batch batch = llama_batch_get_one(prompt_tokens.data(), prompt_tokens.size());    
+    
+    // main decode loop
+    std::string generation;
+    const auto t_main_start = ggml_time_us();
+    int n_decode = 0;
+    llama_token new_token_id;
+    for (int n_pos = 0; n_pos + batch.n_tokens < n_prompt + n_predict; ) {
+        // evaluate the current batch with the transformer model
+        if (llama_decode(ctx, batch)) {
+            fprintf(stderr, "%s : failed to eval, return code %d\n", __func__, 1);
+            return NULL;
+        }
+
+        n_pos += batch.n_tokens;
+
+        // sample the next token
+        {
+            new_token_id = llama_sampler_sample(sampler, ctx, -1);
+
+            // is it an end of generation?
+            if (llama_vocab_is_eog(vocab, new_token_id)) {
+                break;
+            }
+
+            char buf[128];
+            int n = llama_token_to_piece(vocab, new_token_id, buf, sizeof(buf), 0, true);
+            if (n < 0) {
+                fprintf(stderr, "%s: error: failed to convert token to piece\n", __func__);
+                return NULL;
+            }
+            std::string s(buf, n);
+            generation += s;
+            printf("%s", s.c_str());
+            fflush(stdout);
+
+            // prepare the next batch with the sampled token
+            batch = llama_batch_get_one(&new_token_id, 1);
+
+            n_decode += 1;
+        }
+    }
+    printf("\n");
+    const auto t_main_end = ggml_time_us();    
+    
+    // log stats
+    fprintf(stderr, "%s: decoded %d tokens in %.2f s, speed: %.2f t/s\n",
+            __func__, n_decode, (t_main_end - t_main_start) / 1000000.0f, n_decode / ((t_main_end - t_main_start) / 1000000.0f));    
+    fprintf(stderr, "\n");
+    llama_perf_sampler_print(sampler);
+    llama_perf_context_print(ctx);
+    fprintf(stderr, "\n");    
+
+    // free resources
+    llama_sampler_free(sampler);
+    llama_free(ctx);
+
+    return (char*) generation.c_str();
+}
+
+FFI_PLUGIN_EXPORT void start_llama(char* path_model) {
+   
+    ggml_backend_load_all();
+
+    // initialize the model
+    struct llama_model_params model_params = llama_model_default_params();
+    model_params.n_gpu_layers = 99; // use all available GPU layers (if any)
+    model = llama_model_load_from_file(path_model, model_params);
+
+    // initialize vocabulary
+    vocab = llama_model_get_vocab(model);
+
+}
