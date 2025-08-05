ecb439bb23a2de5d2fb62c3fd3f0e6f6a0ea8bee
Shane Lenagh <shanelenagh@gmail.com>
2025-08-05 10:41:00 -0500

    Fixed an API bug and made some names better

--- lib/llama_engine.dart
+++ lib/llama_engine.dart
@@ -9,14 +9,14 @@ class LlamaEngine {
   final _log = Logger((LlamaEngine).toString());
   final avarex_llama.AvarexLlamaBindings _llamacppLib = avarex_llama.AvarexLlamaBindings(ffi.DynamicLibrary.open("llama.dll"));
 
-  LlamaEngine(String modelPath, ffi.Pointer<avarex_llama.llama_model_params>? i_model_params) {   
+  LlamaEngine(String modelPath, ffi.Pointer<avarex_llama.llama_model_params>? modelParams) {   
     _log.info("Starting llama.cpp with model $modelPath...");
-    _llamacppLib.start_llama(_dart2ffi(modelPath), i_model_params ?? ffi.nullptr);
+    _llamacppLib.start_llama(_dart2ffi(modelPath), modelParams ?? ffi.nullptr);
     _log.info("Started llama.cpp");
   }
 
-  String runGeneration(String prompt, int nPredict, ffi.Pointer<avarex_llama.llama_context_params>? i_context_params, ffi.Pointer<avarex_llama.llama_sampler_chain_params>? i_sampler_params) {
-    return _ffi2dart(_llamacppLib.run_generation(_dart2ffi(prompt), nPredict, i_context_params ?? ffi.nullptr, ffi.nullptr ?? ffi.nullptr));
+  String runGeneration(String prompt, int nPredict, ffi.Pointer<avarex_llama.llama_context_params>? contextParams, ffi.Pointer<avarex_llama.llama_sampler_chain_params>? samplerParams) {
+    return _ffi2dart(_llamacppLib.run_generation(_dart2ffi(prompt), nPredict, contextParams ?? ffi.nullptr, samplerParams ?? ffi.nullptr));
   }
   
   @pragma("vm:prefer-inline")
