7d0c1be276d3d8be58049be730bd152fadd95372
Shane Lenagh <shanelenagh@gmail.com>
2025-08-11 20:29:03 -0500

    Pull LLM model from config file

--- example/lib/src/llama_controller.dart
+++ example/lib/src/llama_controller.dart
@@ -1,14 +1,20 @@
+import 'package:avarex_llama_example/config/app_configuration.dart';
 import 'package:vaden/vaden.dart';
 import 'package:avarex_llama/llama_engine.dart' as avarex_llama;
 
 @Api(tag: 'Llama', description: 'Llama LLM Service')
 @Controller('/llama')
 class LlamaController {
-  static final avarex_llama.LlamaEngine llama =  avarex_llama.LlamaEngine("granite-3.3-2b-instruct-Q5_1.gguf", null);
+
+  late final avarex_llama.LlamaEngine llama;
+
+  LlamaController(ApplicationSettings settings) {
+    llama =  avarex_llama.LlamaEngine(settings["aiModels"]["llm"], null);
+  }
 
   @Get('/ask')
   @ApiOperation(summary: 'Ask the LLM a question', description: 'Send a question to the LLM and receive a response.')
-  String ask(@Query("question") @ApiQuery(description: "Question to ask the LLM (i.e., prompt)") question) {
+  String ask(@Query() @ApiQuery(description: "Question to ask the LLM (i.e., prompt)") question) {
     return llama.runGeneration(question, 1000, null, null);
   }
 }
