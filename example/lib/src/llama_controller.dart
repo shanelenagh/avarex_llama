0a08e020c5fc5a5854de78687e00be7394626ef3
Shane Lenagh <shanelenagh@gmail.com>
2025-08-11 21:14:33 -0500

    Fixed redundant re-initialization of llama.cpp engine

--- example/lib/src/llama_controller.dart
+++ example/lib/src/llama_controller.dart
@@ -6,15 +6,15 @@ import 'package:avarex_llama/llama_engine.dart' as avarex_llama;
 @Controller('/llama')
 class LlamaController {
 
-  late final avarex_llama.LlamaEngine llama;
+  static avarex_llama.LlamaEngine? _llama;
 
   LlamaController(ApplicationSettings settings) {
-    llama =  avarex_llama.LlamaEngine(settings["aiModels"]["llm"], null);
+    _llama =  _llama ?? avarex_llama.LlamaEngine(settings["aiModels"]["llm"], null);
   }
 
   @Get('/ask')
   @ApiOperation(summary: 'Ask the LLM a question', description: 'Send a question to the LLM and receive a response.')
   String ask(@Query() @ApiQuery(description: "Question to ask the LLM (i.e., prompt)") question) {
-    return llama.runGeneration(question, 1000, null, null);
+    return _llama?.runGeneration(question, 1000, null, null) ?? "";
   }
 }
