11799c443ae1104b3482d1c38b049a9da62bd54e
Shane Lenagh <shanelenagh@gmail.com>
2025-08-13 13:49:30 -0500

    Simpler main method example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,18 +1,13 @@
-import 'package:avarex_llama/llama_engine.dart' as avarex_llama;
-import 'package:logging/logging.dart';
+import 'package:avarex_llama/llama_engine.dart' as dllama;
+import 'dart:io' as io;
 
-final log = Logger("avarex_llama_example");
 
-void main() {
-  Logger.root.onRecord.listen((record) {
-    // ignore: avoid_print
-    print('${record.time} ${record.level.name} ${record.loggerName} - ${record.message}');
-  });
-  log.info("Starting llama...");
-  final llama = avarex_llama.LlamaEngine("granite-3.3-2b-instruct-Q5_1.gguf", null);
-  log.info("Started llama");
-  final String answer = llama.runGeneration("What height does class A airspace begin at?", 20, null, null);
-  log.info("Got Answer in Dart/Flutter: $answer");
-  final String answer2 = llama.runGeneration("What is the height of class G airspace?", 20, null, null);
-  log.info("Got Answer in Dart/Flutter: $answer2");  
+void main(List<String> args) {
+  if (args.length < 3 || !io.File(args[0]).existsSync() || int.tryParse(args[1]) == null || args[2].isEmpty) { 
+    io.stderr.writeln('Usage: main.dart <gguf_model_path> <max_number_of_tokens> <prompt/question>');
+    io.exit(1);
+  }
+
+  final llama = dllama.LlamaEngine(args[0], null);
+  io.stdout.writeln("Answer:\n${llama.runGeneration(args[2], int.parse(args[1]), null, null)}");
 }
