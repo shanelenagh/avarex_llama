d864a1a0119dca0033541980e2c65ece03f34bcd
Shane Lenagh <shanelenagh@gmail.com>
2025-08-20 10:26:15 -0500

    Changing example to allow for no single/initial prompt to be passed, but rather they can enter multiple questions via stdin (keeping model loaded).

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,11 +3,24 @@ import 'dart:io' as io;
 
 
 void main(List<String> args) {
-  if (args.length < 3 || !io.File(args[0]).existsSync() || int.tryParse(args[1]) == null || args[2].isEmpty) { 
-    io.stderr.writeln('Usage: main.dart <gguf_model_path> <max_number_of_tokens> <prompt/question>');
+  if (args.length < 2 || !io.File(args[0]).existsSync() || int.tryParse(args[1]) == null) { 
+    io.stderr.writeln('Usage: main.dart <gguf_model_path> <max_number_of_tokens> [<prompt/question>]');
     io.exit(1);
   }
 
   final llama = dllama.LlamaEngine(args[0], null);
-  io.stdout.writeln("Answer:\n${llama.runGeneration(args[2], int.parse(args[1]), null, null)}");
+  if (args.length > 2) {
+    io.stdout.writeln("Answer:\n${llama.runGeneration(args[2], int.parse(args[1]), null, null)}");
+  } else {
+    String? prompt;
+    while(true) {
+      io.stdout.write("Enter question/prompt: ");
+      prompt = io.stdin.readLineSync();
+      if (prompt != null && prompt.trim().isNotEmpty) {
+        io.stdout.writeln("Response:\n${llama.runGeneration(prompt, int.parse(args[1]), null, null)}");
+      } else {
+        break;
+      }
+    }
+  }
 }
